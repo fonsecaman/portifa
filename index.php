23c81487465c031b17bbdb3dfd98596c7de4ecdc
Thiago de Lima Gualberto <tdelima@gmail.com>
2021-02-24 17:21:11 -0300

    menu fixo reponsivo mas comentado

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Thiago Gualberto - Product Designer</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;700&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body class="template-black"> 
+</body>
+</html>
